8a2be461c6421b6b0a66c5586bb4aa0df05b2d3f
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2025-05-12 19:04:07 +0300

    few changes
    changed the name of mainwindow to loginwindow;
    the icon of the login window doesn't appear and the window doesn't appear

--- RestaurantOrders/Views/LoginWindow.xaml.cs
+++ RestaurantOrders/Views/LoginWindow.xaml.cs
@@ -14,9 +14,9 @@ namespace RestaurantOrders.Views
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginWindow : Window
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
         }
